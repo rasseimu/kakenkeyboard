babed96142cdd5a9f174e2855a058e8f57927168
DESKTOP-UTKV4FT\julee <s20082@g.hst.titech.ac.jp>
2022-05-13 19:27:18 +0900

    littlechange

--- keyboard/structtest.py
+++ keyboard/structtest.py
@@ -5,3 +5,9 @@ eachdata_type = [( 'key' , 'U10' ),( 'num' , 'i4' ),( 'det' , 'U10' )]
 eachdatas = np.array([('C',4,'half'),('D',4,'half')], dtype = eachdata_type)
 print(eachdatas[0])
 print(eachdatas[0]['key'])
+
+
+eachdatas[0]['key'] = 'E'
+
+print(eachdatas[0])
+print(eachdatas[0]['key'])
