7ffa7f6d95ec9d4e93f2ca7d87b7d5b0fda5fabc
DESKTOP-UTKV4FT\julee <s20082@g.hst.titech.ac.jp>
2022-07-27 08:54:20 +0900

    alfa

--- keyboard/wiretest/operand.py
+++ keyboard/wiretest/operand.py
@@ -2,19 +2,37 @@ a = [0,1,3,4,7,'D5']
 b = [4,6,2,'F4','G3','D5']
 flag = 0
 h = 0
+t = 0
+c = []
 
-print(flag)
+#print(flag)
+print(c)
+c = a.remove(0)
+print(c)
+print(a)
 
 
 
-while a[h] != '\0':
+"""list = [[]*2]*3
+print(list)
+二次元配列宣言"""
+
+
+"""if isinstance(a[0], list):
+    print('good')
+    i used this"""
+
+
+"""while a[h] != '\0':
     print(a[h])
     h += 1
+    it didn't work"""
 
 """for i in range(len(a)):
     for j in range(6):
         if a[i] == b[j]:
             flag += 1
-        print(i,j,flag)"""
+        print(i,j,flag)
+        it didn't work"""
 
-print(flag)
+#print(flag)
--- wire.py
+++ wire.py
@@ -1,7 +1,7 @@
 import numpy as np
 import statistics
-output = [0,0,0,0,0,0,0,0,0,0,0,'C4','C4','C4','C4','C4','C4','D4','D4','D4','D4','D4','D4','D4','D4','D4','D4','D4',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'C3','C3','C3','C3','C3','C3','C3','C3','C3','C3','C3','C3','C3','C3',0,0,0,0,0,0,0,0,0,0]
-Musicscore = []
+output = [0,0,0,0,0,'D4','D4','D4','D4','D4','D4','D4',0]
+Musicscore = [[0]*10]*10
 i = 0
 j = 1
 k = 0
@@ -14,12 +14,14 @@ def Rest(k,x):
     Musicscore[k][1] = x
 
 def Sound(k,t,j,x):
+    print(k,t,j,x)
     Musicscore[k][2*t] = output[j-1][t]
     Musicscore[k][2*t+1] = x
 
 #先頭要素"0"の削除
 while output[0] == 0:
     output.remove(0)
+    #print(output)
 
 
 
@@ -47,56 +49,59 @@ for j in range(len(output)):#貰った配列の長さ
         n += 1
 
     #単音の場合の処理
-    elif output[j-1][1] == None:#配列に宣言されいないところを指定してはいけない。
-        if n > 900/BPM*0.8 & n < 900/BPM*1.5:
+    elif not isinstance(output[j-1], list):
+        n += 1
+        if (n > 900/BPM*0.8) & (n < 900/BPM*1.5):
+            print(j,n)
             if output[j-1] == 0:
                 Rest(k,0.5)
             else:
+                print(k,j)
                 Sound(k,0,j,0.5)
         
-        elif n == 900/BPM*1.5 | n > 900/BPM*1.5 & n < 900/BPM*3:
+        elif ((n == 900/BPM*1.5) | (n > 900/BPM*1.5)) & (n < 900/BPM*3):
             if output[j-1] == 0:
                 Rest(k,1)
             else:
                 Sound(k,0,j,1)
         
-        elif n == 900/BPM*3 | n > 900/BPM*3 & n < 900/BPM*6:
+        elif ((n == 900/BPM*3) | (n > 900/BPM*3)) & (n < 900/BPM*6):
             if output[j-1] == 0:
                 Rest(k,2)
             else:
                 Sound(k,0,j,2)
         
-        elif n == 900/BPM*6 | n > 900/BPM*6 & n < 900/BPM*10:
+        elif ((n == 900/BPM*6) | (n > 900/BPM*6)) & (n < 900/BPM*10):
             if output[j-1] == 0:
                 Rest(k,4)
             else:
                 Sound(k,0,j,4)
 
         n = 0
+        k += 1
 
     #複音の場合の処理
     else:
-        if n > 900/BPM*0.8 & n < 900/BPM*1.5:
-            for t in output[j-1]:
+        n += 1
+        if (n > 900/BPM*0.8) & (n < 900/BPM*1.5):
+            for t in range(len(output[j-1])):
                 Sound(k,t,j,0.5)
         
-        elif (n == 900/BPM*1.5 | n > 900/BPM*1.5) & n < 900/BPM*3:
-            for t in output[j-1]:
+        elif ((n == 900/BPM*1.5) | (n > 900/BPM*1.5)) & (n < 900/BPM*3):
+            for t in range(len(output[j-1])):
                 Sound(k,t,j,1)
         
-        elif (n == 900/BPM*3 | n > 900/BPM*3) & n < 900/BPM*6:
-            for t in output[j-1]:
+        elif ((n == 900/BPM*3) | (n > 900/BPM*3)) & (n < 900/BPM*6):
+            for t in range(len(output[j-1])):
                 Sound(k,t,j,2)
         
-        elif (n == 900/BPM*6 | n > 900/BPM*6) & n < 900/BPM*10:
-            for t in output[j-1]:
+        elif ((n == 900/BPM*6) | (n > 900/BPM*6)) & (n < 900/BPM*10):
+            for t in range(len(output[j-1])):
                 Sound(k,t,j,4)
 
-        t = 0
+        t += 1
         n = 0
 
 
-
-    k += 1  
 
 print(Musicscore)
